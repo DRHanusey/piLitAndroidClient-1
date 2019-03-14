1982ee0348308fd176562f9444d1f5e0e44fcb86
Malik Vankirk <tug38802@temple.edu>
2019-03-14 16:40:07 -0400

    added home activity and XML

--- app/src/main/java/edu/temple/pilitandroidclient/Login.java
+++ app/src/main/java/edu/temple/pilitandroidclient/Login.java
@@ -1,13 +1,28 @@
 package edu.temple.pilitandroidclient;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.buttonLogin);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
