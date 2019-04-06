cd0efa4f3bca8855afc5b09c59f5c886d6123387
Daniel Hanusey <tud51324@gmail.com>
2019-04-06 10:55:36 -0400

    changed time stamp from arry to arraylist

--- app/src/main/java/edu/temple/pilitandroidclient/Objects/Command.java
+++ app/src/main/java/edu/temple/pilitandroidclient/Objects/Command.java
@@ -1,14 +1,17 @@
 package edu.temple.pilitandroidclient.Objects;
 
+import java.util.ArrayList;
+
 public class Command {
-    int range[];                        //range of LED lights being changed, each item is an individual light
     enum effect                         //name of light pattern effect
     {
-        RAINBOW, CUSTOM
+        RAINBOW, CUSTOM, SOLID, FLASH
     }
+
+    int range[];                        //range of LED lights being changed, each item is an individual light
     int brightness;                     //level of brightness for all lights unchanged by command
     Color otherLights = new Color();    //color that all lights unchanged by command display
-    Timestamp timestampArray[];         //an array of Timestamp objects(time, color, brightness); each object represents color at a particular time
+    ArrayList<Timestamp> timestampArray;         //an array of Timestamp objects(time, color, brightness); each object represents color at a particular time
 
 
 
